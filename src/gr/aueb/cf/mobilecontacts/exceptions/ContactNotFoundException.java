6c5abbe03499030c8acb7cd767abc78e052c8a81
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2025-01-28 09:12:51 +0200

    First commit

---
+++ src/gr/aueb/cf/mobilecontacts/exceptions/ContactNotFoundException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.mobilecontacts.exceptions;
+
+public class ContactNotFoundException extends Exception{
+
+
+    public ContactNotFoundException (String message) {
+        super(message);
+    }
+}
