5e4c430bc7a6f7622a616fe5f8beb7388323d6b5
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2025-01-28 20:53:40 +0200

    Controller

---
+++ src/gr/aueb/cf/mobilecontacts/controller/MobileContactController.java
@@ -0,0 +1,51 @@
+package gr.aueb.cf.mobilecontacts.controller;
+
+import gr.aueb.cf.mobilecontacts.dao.IMobileContactDAO;
+import gr.aueb.cf.mobilecontacts.dao.MobileContactDAOImpl;
+import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
+import gr.aueb.cf.mobilecontacts.dto.MobileContactReadOnlyDTO;
+import gr.aueb.cf.mobilecontacts.dto.MobileContactUpdateDTO;
+import gr.aueb.cf.mobilecontacts.exceptions.PhoneNumberAlreadyExistsException;
+import gr.aueb.cf.mobilecontacts.model.MobileContact;
+import gr.aueb.cf.mobilecontacts.service.IMobileContactService;
+import gr.aueb.cf.mobilecontacts.service.MobileContactServiceImpl;
+import gr.aueb.cf.mobilecontacts.validation.ValidationUtil;
+
+public class MobileContactController {
+
+    private final IMobileContactDAO dao = new MobileContactDAOImpl();
+    private final IMobileContactService service = new MobileContactServiceImpl(dao);
+
+    public String insertContact(MobileContactInsertDTO insertDTO) {
+        MobileContact mobileContact;
+        MobileContactReadOnlyDTO readOnlyDTO;
+
+        try {
+            // validate input data
+            String errorVector = ValidationUtil.validateDTO(insertDTO);
+            if (!errorVector.isEmpty()) {
+                return "Error." + "Validation error\n" + errorVector;
+            }
+
+            // if validation is ok, then insert contact
+
+            mobileContact = service.insertMobileContact(insertDTO);
+            readOnlyDTO = mapMobileContactToDTO(mobileContact);
+            return "OK\n" + serializeDTO(readOnlyDTO);
+        }catch (PhoneNumberAlreadyExistsException e) {
+            return "Error\n" + e.getMessage() + "\n";
+        }
+
+    }
+
+    private MobileContactReadOnlyDTO mapMobileContactToDTO(MobileContact mobileContact) {
+        return new MobileContactReadOnlyDTO(mobileContact.getId(), mobileContact.getFirstname(),
+                mobileContact.getFirstname(), mobileContact.getPhoneNumber());
+    }
+
+
+    private String serializeDTO(MobileContactReadOnlyDTO readOnlyDTO) {
+        return "ID: " + readOnlyDTO.getId() + ", Ονομα: " + readOnlyDTO.getFirstname()
+                + ", Επωνυμο: " + readOnlyDTO.getLastname() + ", Τηλ. Αριθμος: " + readOnlyDTO.getPhoneNumber();
+    }
+}
