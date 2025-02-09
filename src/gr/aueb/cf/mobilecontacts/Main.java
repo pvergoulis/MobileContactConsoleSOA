87c27c12f6eba1533e360a37b9840f3bd943e57e
Pavlos Vergoulis <vergoulispaulos@gmail.com>
2025-02-09 14:08:05 +0200

    Main

--- src/gr/aueb/cf/mobilecontacts/Main.java
+++ src/gr/aueb/cf/mobilecontacts/Main.java
@@ -2,40 +2,43 @@ package gr.aueb.cf.mobilecontacts;
 
 import gr.aueb.cf.mobilecontacts.controller.MobileContactController;
 import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
+import gr.aueb.cf.mobilecontacts.dto.MobileContactUpdateDTO;
+import gr.aueb.cf.mobilecontacts.exceptions.ContactNotFoundException;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner in = new Scanner(System.in);
     private static final MobileContactController controller = new MobileContactController();
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ContactNotFoundException {
         String choice;
 
-        while(true){
+        while (true) {
             printMenu();
             choice = getToken();
 
-            if (choice.equals("q") || (choice.equals("Q"))){
+            if (choice.equals("q") || (choice.equals("Q"))) {
                 break;
             }
 
             handleChoice(choice);
         }
-        System.out.println("Thank your for using Mobile Contact App");
+
+        System.out.println("Thank you for using Mobile Contacts App");
     }
 
-
-    public static void handleChoice(String choice) {
+    public static void handleChoice(String choice) throws ContactNotFoundException {
         String firstname;
         String lastname;
         String phoneNumber;
         String response;
+        long id;
 
         switch (choice) {
             case "1":
-                System.out.println("Παρακαλώ εισάγεται Όνομα, Επώνυμο , Αρ. Τηλεφώνου");
+                System.out.println("Παρακαλώ εισάγετε Όνομα, Επώνυμο, Αρ. Τηλεφώνου");
                 firstname = getToken();
                 lastname = getToken();
                 phoneNumber = getToken();
@@ -43,34 +46,74 @@ public class Main {
                 response = controller.insertContact(insertDTO);
 
                 if (response.startsWith("OK")) {
-                    System.out.println("Επιτυχείς Εισαγωγή");
+                    System.out.println("Επιτυχής Εισαγωγή");
                     System.out.println(response.substring(3));
-                }else {
+                } else {
                     System.out.println("Ανεπιτυχής Εισαγωγή");
                     System.out.println(response.substring(7));
                 }
                 break;
             case "2":
-                //
+                System.out.println("Εισάγετε Αριθμό Τηλεφώνου");
+                phoneNumber = getToken();
+                response = controller.getContactByPhoneNumber(phoneNumber);
+                if (response.startsWith("Error")) {
+                    System.out.println("Η επαφή δεν βρέθηκε.");
+                    System.out.println(response.substring(3));
+                    return;
+                }
+                System.out.println("Ανεπιτυχής Εισαγωγή");
+                System.out.println(response.substring(6));
+                System.out.println("Εισάγετε το υπάρχον ID");
+                long oldId = Long.parseLong(getToken());
+                System.out.println("Παρακαλώ εισάγετε νέο όνομα");
+                firstname = getToken();
+                System.out.println("Παρακαλώ εισάγετε νέο επώνυμο");
+                lastname = getToken();
+                System.out.println("Παρακαλώ εισάγετε νέο τηλεφωνικό αριθμό");
+                phoneNumber = getToken();
+                MobileContactUpdateDTO mobileContactUpdateDTO = new MobileContactUpdateDTO(oldId, firstname, lastname, phoneNumber);
+                response = controller.updateContact(mobileContactUpdateDTO);
+                System.out.println(response);
                 break;
             case "3":
-                //
+                System.out.println("Εισάγετε Κωδικό Επαφής");
+                id = Long.parseLong(getToken());
+                response = controller.deleteContactById(id);
+                if (response.startsWith("OK")) {
+                    System.out.println("Επιτυχής Διαγραφή");
+                    System.out.println(response.substring(3));
+                } else {
+                    System.out.println("Ανεπιτυχής Διαγραφή");
+                    System.out.println(response.substring(6));
+                }
                 break;
             case "4":
-                //
+                System.out.println("Εισάγετε Κωδικό Επαφής");
+                id =  Long.parseLong(getToken());
+                response = controller.getContactById(id);
+                if (response.startsWith("OK")) {
+                    System.out.println("Επιτυχής Αναζήτηση");
+                    System.out.println(response.substring(3));
+                } else {
+                    System.out.println("Ανεπιτυχής Αναζήτηση");
+                    System.out.println(response.substring(6));
+                }
                 break;
             case "5":
-                //
+                List<String > mobileContacts = controller.getAllContacts();
+                if (mobileContacts.isEmpty()) System.out.println("Κενή λίστα επαφών");
+                mobileContacts.forEach(System.out::println);
                 break;
             default:
-                //
+                System.out.println("Λάθος επιλογή");
                 break;
         }
 
     }
 
     public static void printMenu() {
-        System.out.println("Επιλέξτε ένα από τα παρακάτω");
+        System.out.println("Επιλέξτε ένα από τα παρακάτω:");
         System.out.println("1. Εισαγωγή επαφής");
         System.out.println("2. Ενημέρωση επαφής");
         System.out.println("3. Διαγραφή επαφής");
@@ -79,8 +122,7 @@ public class Main {
         System.out.println("Q/q. Έξοδος");
     }
 
-
-    public static String getToken(){
+    public static String getToken() {
         return in.nextLine().trim();
     }
 }
